4c1ecb5e479c4f4bf034be07798a14e7f0162ff4
Tom Crawley <tomboyyyalt@gmail.com>
2024-06-07 19:26:29 +0100

    sanity commit

---
+++ target/debug/deps/rust_compiler-42d1de14745f23cc.d
@@ -0,0 +1,9 @@
+C:\Users\Tom\Desktop\Coding\Rust\rust-compiler\target\debug\deps\librust_compiler-42d1de14745f23cc.rmeta: src\main.rs src\lex.rs src\parse.rs src\semantic.rs src\code_gen.rs
+
+C:\Users\Tom\Desktop\Coding\Rust\rust-compiler\target\debug\deps\rust_compiler-42d1de14745f23cc.d: src\main.rs src\lex.rs src\parse.rs src\semantic.rs src\code_gen.rs
+
+src\main.rs:
+src\lex.rs:
+src\parse.rs:
+src\semantic.rs:
+src\code_gen.rs:
---
+++ target/debug/deps/rust_compiler-e4adc85e1eb03a2e.d
@@ -0,0 +1,9 @@
+C:\Users\Tom\Desktop\Coding\Rust\rust-compiler\target\debug\deps\librust_compiler-e4adc85e1eb03a2e.rmeta: src\main.rs src\lex.rs src\parse.rs src\semantic.rs src\code_gen.rs
+
+C:\Users\Tom\Desktop\Coding\Rust\rust-compiler\target\debug\deps\rust_compiler-e4adc85e1eb03a2e.d: src\main.rs src\lex.rs src\parse.rs src\semantic.rs src\code_gen.rs
+
+src\main.rs:
+src\lex.rs:
+src\parse.rs:
+src\semantic.rs:
+src\code_gen.rs:
