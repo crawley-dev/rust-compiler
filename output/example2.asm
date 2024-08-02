fb454ba23f66244ac666f725a396749e4a56c0b6
Tom Crawley <tomboyyyalt@gmail.com>
2024-08-02 15:12:15 +0100

    slight error message changes.

--- output/example2.asm
+++ output/example2.asm
@@ -9,9 +9,23 @@ _start:
     mov rsi, 5
     sub rcx, rsi
     add rax, rcx
-    mov byte [rbp-1], rax ; Ident('num')
-    mov byte [rbp-2], 8 ; Ident('num2')
+    mov word [rbp-2], rax ; Ident('num')
+    mov byte [rbp-3], 8 ; Ident('num2')
+    mov byte [rbp-3], 6 ; Ident('test')
+; If
+    mov rax, [rbp-3] ; Token { kind: Ident, value: Some("test"), pos: (7, 7) }
+    mov rcx, 5
+    cmp rax, rcx
+    sete al
+    movzx rax, al
+    cmp rax, 0 
+    je .1_IF_FALSE
 ; Exit Program
-    mov rdi, [rbp-1] ; Token { kind: Ident, value: Some("num"), pos: (8, 6) }
+    mov rdi, 5
+    mov rax, 60
+    syscall
+.1_IF_FALSE:
+; Exit Program
+    mov rdi, [rbp-2] ; Token { kind: Ident, value: Some("num"), pos: (8, 10) }
     mov rax, 60
     syscall
